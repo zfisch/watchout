007df1b14142a761241f57765dd7ef2e9ddac094
zfisch <zacharyrobert@gmail.com>
2015-04-03 19:43:56 -0700

    added mouse moving solo

--- watchout.js
+++ watchout.js
@@ -55,7 +55,6 @@ placeEnemies();
 //define a function to make enemies move every second
 
 var updatePositions = function(){
-  console.log("update");
   enemies.forEach(function(enemy){
     var x = randomNumber();
     var y = randomNumber();
@@ -74,8 +73,60 @@ var updatePositions = function(){
 setInterval(updatePositions, 1000);
 
 
+//create a movable player
+var createHero = function(){
+  var hero = [{
+    "name": "hero",
+    "x": 400,
+    "y": 400
+  }];
+  return hero;
+};
+
+var hero = createHero();
+
+//add a hero to the board
+var placeHero = function(){
+  d3.select("svg").append('image').data(hero)
+  .attr("class", "hero")
+  .attr("xlink:href", "hero.gif")
+  .attr("height", 75)
+  .attr("width", 75)
+  .attr("x", function(d){return d["x"]})
+  .attr("y", function(d){return d["y"]});
+};
+
+placeHero();
+
+//create function to move player
+
+  //use d3.mouse(this) to track mouse
+  d3.select("svg").on("mousemove", function() {
+    var position = d3.mouse(this);
+    var px = position[0]
+    var py = position[1]
+
+    hero["x"] = px;
+    hero["y"] = py;
+
+     d3.select(".hero").data(hero)
+    .attr("x", px)
+    .attr("y", py);
+  });
+    //leave hero where mouse moved outside of board???
+    //place hero where mouse moves onto the board???
 
 
+
+//keep score
+
+//high score
+
+//implement collision detection
+
+  //increment collision counter
+
+  //reset score
 
 
 
