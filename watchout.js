2a69dca602c1cba41f292f27be73c2f2874818e5
Zack Fischmann <zacharyrobert@gmail.com>
2015-04-04 17:17:46 -0700

    total refactor

--- watchout.js
+++ watchout.js
@@ -1,174 +1,122 @@
-// start slingin' some d3 here.
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
 
-//create a board
+//general settings of game
+var settings = {
+  w: window.innerWidth,
+  h: window.innerHeight,
+  r: 15,
+  n: 30,
+  duration: 1500
+};
 
-var size = 800;
+//defaults
 var score = 0;
 var highScore = 0;
-var collisionCounter = 0;
+var collisionCount = 0;
+var hero = { x: settings.w/2, y: settings.h/2 };
 
-var gameBoard = d3.select('body').append('svg')
-  .attr('height', size + "px")
-  .attr('width', size + "px");
+//helper functions
+var pixelize = function(num){ return num + 'px'; };
 
-//generate random number
-var randomNumber = function(){
-  return Math.floor(Math.random() * Math.floor(size * .80));
+var randomNumber = function(n){ return Math.floor( Math.random() * n ) ; };
+var randomX = function(){ return pixelize( randomNumber(settings.w - settings.r*2) ) ; };
+var randomY = function(){ return pixelize( randomNumber(settings.h - settings.r*2) ) ; };
+
+var updateScore = function(){
+  d3.select(".high span").text(highScore);
+  d3.select(".current span").text(score);
+  d3.select(".collisions span").text(collisionCount);
 };
 
-//define a function for creating enemies
-var createEnemies = function(n){
-  var x, y, enemy, arrayOfEnemies = [];
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
 
-  //create enemy objects with unique names and positions
-  for(var i = 0; i < n; i++){
-    //define starting positions for each enemy
-    x = randomNumber();
-    y = randomNumber();
-    //create enemy object
-    enemy = {
-      "name": i,
-      "x": x,
-      "y": y
-    };
-
-    //push enemy object to array of enemies
-    arrayOfEnemies.push(enemy);
-  }
-  return arrayOfEnemies;
-};
-
-//create enemies
-var enemies = createEnemies(10);
-
-
-
-var placeEnemies = function(){
-  d3.select("svg").selectAll("image").data(enemies)
-  .enter()
-  .append("svg:image")
-  .attr("class", "enemy")
-  .attr("xlink:href", "asteroid.png")
-  .attr("height", 75)
-  .attr("width", 75)
-  .attr("x", function(d){return d["x"]})
-  .attr("y", function(d){return d["y"]});
-};
-placeEnemies();
-
-var enemyHeight = d3.select('.enemy').attr('height');
-var enemyWidth = d3.select('.enemy').attr('width');
-
-//define a function to make enemies move every second
-
-var updatePositions = function(){
-  enemies.forEach(function(enemy){
-    var x = randomNumber();
-    var y = randomNumber();
-    enemy['x'] = x;
-    enemy['y'] = y;
-  });
-
-   d3.select("svg").selectAll("image").data(enemies)
-  .transition()
-  .duration(750)
-  .attr("x", function(d){return d["x"]})
-  .attr("y", function(d){return d["y"]});
-};
-
-
-setInterval(updatePositions, 1000);
-
-
-//create a movable player
-var createHero = function(){
-  var hero = {
-    "name": "hero",
-    "x": 400,
-    "y": 400
-  };
-  return hero;
-};
-
-var hero = createHero();
-
-//add a hero to the board
-var placeHero = function(){
-  d3.select("svg").append('image').data(hero)
-  .attr("class", "hero")
-  .attr("xlink:href", "hero.gif")
-  .attr("height", 75)
-  .attr("width", 75)
-  .attr("x", function(d){return d["x"]})
-  .attr("y", function(d){return d["y"]});
-};
-
-placeHero();
-var heroHeight = d3.select(".hero").attr("height");
-var heroWidth = d3.select(".hero").attr("width");
-
-//move player with mouseover
-d3.select("svg").on("mousemove", function() {
-  var position = d3.mouse(this);
-  var px = position[0] - (heroWidth / 2);
-  var py = position[1] - (heroHeight / 2);
-
-  hero[0]["x"] = px;
-  hero[0]["y"] = py;
-
-   d3.select(".hero").data(hero)
-  .attr("x", px)
-  .attr("y", py);
+//Game Board settings
+var gameBoard = d3.select('.gameBoard').style({
+  height: pixelize(settings.h),
+  width: pixelize(settings.w)
 });
 
 
-//find position of an object on the screen
-var findPosition = function(obj){
-  var x = obj.attr('x');
-  var y = obj.attr('y');
-  var position = [x, y];
-  return position;
-};
+//hero settings
+d3.select('.hero').style({
+  top: pixelize(hero.x),
+  left: pixelize(hero.y),
+  width: pixelize(settings.r),
+  height: pixelize(settings.r),
+  'border-radius': pixelize(settings.r)
+});
+
+//mouse controls
+gameBoard.on('mousemove', function(){
+  var loc = d3.mouse(this);
+  hero = { x: loc[0], y: loc[1] };
+  d3.select('.hero').style({
+    top: pixelize(hero.y - settings.r/2),
+    left: pixelize(hero.x - settings.r/2)
+  });
+});
+
+//enemy placement and settings
+var enemies = d3.selectAll('.enemies')
+  .data(d3.range(settings.n))
+  .enter().append('div')
+  .attr('class', 'enemy')
+  .style({
+    top: randomY,
+    left: randomX,
+    width: (pixelize(settings.r*2)),
+    height: (pixelize(settings.r*2))
+  });
+
+// enemy positioning
+var updatePosition = function(obj){
+  obj.transition().duration(settings.duration).style({
+    top: randomY,
+    left: randomX
+  }).each('end', function(){
+    updatePosition( d3.select(this) );
+  });
+}
+
+updatePosition(enemies);
 
 
-//implement collision detection
-var detectCollision = function(){
+//score counter
+var scoreTicker = function(){
+  score++;
+  highScore = Math.max(score, highScore);
+  updateScore();
+}
 
- enemies.forEach(function(enemy){
-  var diffx = Math.abs(hero[0]["x"] - (enemy["x"] - enemyWidth));
-  var diffy = Math.abs(hero[0]["y"] - (enemy["y"] - enemyHeight));
+setInterval(scoreTicker, 100);
 
+//collision handling
+var prevCollision = false;
 
-  if(diffx < ((enemyWidth/2) + (heroWidth/2)) ||
-     diffy < ((enemyHeight/2) + (heroHeight/2)) ){
-  //increment collision counter
-    collisionCounter++;
-    console.log(collisionCounter);
-  //reset score
-    resetScore();
-    return;
-   // console.log(collisionCounter);
+var detectCollisions = function(){
+
+  var collision = false;
+
+  enemies.each(function(){
+    var cx = this.offsetLeft + settings.r;
+    var cy = this.offsetTop  + settings.r;
+    // the magic of collision detection
+    var x = cx - hero.x;
+    var y = cy - hero.y;
+    if( Math.sqrt(x*x + y*y) < settings.r*2 ){
+      collision = true;
+    }
+  });
+
+  if(collision) {
+    score = 0;
+    if( prevCollision != collision){
+      collisionCount = collisionCount + 1;
+    }
   }
- })
+  prevCollision = collision;
 };
 
-//increment score
-setInterval(function(score){
-  detectCollision();
-  score++
-}, 250);
-
-var resetScore = function(){
-  if(score > highScore){
-    highScore = score;
-  }
-  score = 0;
-};
-
-//high score
-
-
-
-
-
-
+d3.timer(detectCollisions);
